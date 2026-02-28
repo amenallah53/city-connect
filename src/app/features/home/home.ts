f8b45fa948b64f951038a0099c7841b6570de469
amenallah53 <amenkalai53@gmail.com>
2026-03-01 00:12:41 +0100

    aboutus  section done !!

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HeroSection } from '../../shared/components/home/hero-section/hero-section';
 import { KeyServicesSection } from '../../shared/components/home/key-services-section/key-services-section';
+import { AboutUs } from '../../shared/components/home/about-us/about-us';
 
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.html',
   styleUrl: './home.css',
-  imports: [HeroSection, KeyServicesSection],
+  imports: [HeroSection, KeyServicesSection, AboutUs],
   standalone: true
 })
 export class Home {
