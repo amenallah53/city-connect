4e98aee6bb322858dd216bac0bce688492405c39
Mohamed El Habib El Fekih <habib.elfekih2004@gmail.com>
2026-03-17 03:23:18 +0100

    all the restyling is done

--- src/app/features/create-account/create-account.ts
+++ src/app/features/create-account/create-account.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { LoginProjetDescrip } from '../../shared/components/login-projet-descrip/login-projet-descrip';
-
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-create-account',
-  imports: [LoginProjetDescrip],
+  imports: [LoginProjetDescrip,RouterModule],
   templateUrl: './create-account.html',
   styleUrl: './create-account.css',
 })
