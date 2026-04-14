2ab06bfcfc4f69a8e1174a26079b876ac1a01cd1
Ghazi Meddeb <ghazimdb@gmail.com>
2026-04-14 19:08:57 +0100

    updated the angular version and istalled prime ng

--- admin/src/app/shared/components/header/header.ts
+++ admin/src/app/shared/components/header/header.ts
@@ -1,11 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { Router, RouterModule, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
-  imports: [CommonModule, RouterModule],
+  imports: [RouterModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
