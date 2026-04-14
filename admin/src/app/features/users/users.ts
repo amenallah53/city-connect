708b4bcd8851bcb91be10d89b9bba005e98bf29d
Ghazi Meddeb <ghazimdb@gmail.com>
2026-04-14 19:35:36 +0100

    tiny adjustments to the sidebar and the header to make them easier to work around, added pagination to the users page

--- admin/src/app/features/users/users.ts
+++ admin/src/app/features/users/users.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { Complaints } from "../complaints/complaints";
 import { UserCard } from '../../shared/components/cards/user-card/user-card';
+import { Paginator, PaginatorModule, PaginatorState } from 'primeng/paginator';
 
 @Component({
   selector: 'app-users',
-  imports: [Complaints, UserCard],
+  imports: [UserCard, PaginatorModule],
   templateUrl: './users.html',
   styleUrl: './users.css',
 })
 export class Users {
+  first: number = 0;
+  rows: number = 10;
 
+  onPageChange(event: PaginatorState) {
+    this.first = event.first ?? 0;
+    this.rows = event.rows ?? 10;
+  }
 }
